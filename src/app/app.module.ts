3d43d9130fc9c34e2c7da072dd946ac929a6979f
yassine raddaoui <yassineraddaouicr7@gmail.com>
2023-11-27 00:45:53 +0100

    logout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { AuthInterceptor } from './components/shareed/auth.interceptor';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { FormsModule } from '@angular/forms';
@@ -13,7 +12,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CheckEmailComponent } from './components/auth/check-email/check-email.component';
 import { ResetPasswordComponent } from './components/auth/reset-password/reset-password.component';
 import {ForgotPasswordComponent} from "./components/auth/forgot-password/forgot-password.component";
-import {ToastrModule} from "ngx-toastr"; // Import ReactiveFormsModule
+import {ToastrModule} from "ngx-toastr";
+import {AuthInterceptor} from "./components/shareed/interceptors/auth.interceptor";
+import {AddRideComponent} from "./components/driver/add-ride/add-ride.component";
+import {VerifyAccountComponent} from "./components/auth/verify-account/verify-account.component";
+import {HeaderComponent} from "./components/layout/header/header.component";
+import {FooterComponent} from "./components/layout/footer/footer.component";
+import {SpinnerComponent} from "./components/shareed/spinner/spinner.component"; // Import ReactiveFormsModule
 
 @NgModule({
   declarations: [
@@ -23,6 +28,11 @@ import {ToastrModule} from "ngx-toastr"; // Import ReactiveFormsModule
     CheckEmailComponent,
     ResetPasswordComponent,
     ForgotPasswordComponent,
+    AddRideComponent,
+    VerifyAccountComponent,
+    HeaderComponent,
+    FooterComponent,
+    SpinnerComponent
 
   ],
   imports: [
