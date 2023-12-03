f53b89c3b72f10203aa9d80bd4dbccd0cd5020ff
yassine raddaoui <yassineraddaouicr7@gmail.com>
2023-12-03 23:37:11 +0100

    fix roles and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AdminDashboardComponent } from './components/admin/adminDashboard/adminDashboard.component';
-import { UserDashboardComponent } from './components/user/userDashboard/userDashboard.component';
-import { NotFoundComponent } from './components/shareed/NotFound/NotFound.component';
-import { DriverDashboardComponent } from './components/driver/driverDashboard/driverDashboard.component';
-import { AuthGuard } from './guards/Auth-guard';
-import { LoginComponent } from './components/auth/login/login.component';
-import { RegisterComponent } from './components/auth/register/register.component';
-import { CheckEmailComponent } from './components/auth/check-email/check-email.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AdminDashboardComponent} from './components/admin/adminDashboard/adminDashboard.component';
+import {UserDashboardComponent} from './components/user/userDashboard/userDashboard.component';
+import {NotFoundComponent} from './components/shareed/NotFound/NotFound.component';
+import {DriverDashboardComponent} from './components/driver/driverDashboard/driverDashboard.component';
+import {AuthGuard} from './guards/Auth-guard';
+import {LoginComponent} from './components/auth/login/login.component';
+import {RegisterComponent} from './components/auth/register/register.component';
+import {CheckEmailComponent} from './components/auth/check-email/check-email.component';
 import {ForgotPasswordComponent} from "./components/auth/forgot-password/forgot-password.component";
 import {ResetPasswordComponent} from "./components/auth/reset-password/reset-password.component";
 import {VerifyAccountComponent} from "./components/auth/verify-account/verify-account.component";
 import {AddRideComponent} from "./components/driver/add-ride/add-ride.component";
-import {HeaderComponent} from "./components/layout/header/header.component";
 import {RideComponent} from "./components/user/ride/ride.component";
 import {AddCarComponent} from "./components/driver/add-car/add-car.component";
 import {ListCarsComponent} from "./components/driver/list-cars/list-cars.component";
 
 const routes: Routes = [
-  { path: 'login',component:LoginComponent},
-  { path: 'register',component:RegisterComponent},
-  {path:'check-email/:email',component:CheckEmailComponent},
-  {path:'verify-account',component:VerifyAccountComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'check-email/:email', component: CheckEmailComponent},
+  {path: 'verify-account', component: VerifyAccountComponent},
 
-  {path:'forgot-password',component:ForgotPasswordComponent},
-  {path:'reset-password/:token',component:ResetPasswordComponent},
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path: 'forgot-password', component: ForgotPasswordComponent},
+  {path: 'reset-password/:token', component: ResetPasswordComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
   {
     path: 'admin', component: AdminDashboardComponent,
     canActivate: [AuthGuard],
@@ -33,53 +32,35 @@ const routes: Routes = [
       role: 'ADMIN'
     }
   },
-  { path: 'driver', component: DriverDashboardComponent,
-  canActivate: [AuthGuard],
-  data: {
-    role: 'DRIVER'
-  }
-},
-  { path: 'rides', component: RideComponent,
-    data: {
-      role: 'USER'
-    }
-
-  },
-
-  {path:'header',component:HeaderComponent},
   {
-    path: 'add-ride', component: AddRideComponent,
+    path: 'driver', component: DriverDashboardComponent,
     canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
     data: {
-      role: 'driver'
-    }
+      role: 'DRIVER'
+    },
+    children: [
+      {path: 'rides', component: RideComponent},
+      {path: 'add-ride', component: AddRideComponent},
+      {path: 'add-car', component: AddCarComponent},
+      {path: 'list-car', component: ListCarsComponent}
+    ]
   },
   {
-    path: 'add-car', component: AddCarComponent,
-    canActivate: [AuthGuard],
-    data: {
-      role: 'driver'
-    }
-  },
-  {
-    path: 'list-car', component: ListCarsComponent,
-    canActivate: [AuthGuard],
-    data: {
-      role: 'driver'
-    }
-  },
-  { path: 'user', component: UserDashboardComponent,
+    path: 'user', component: UserDashboardComponent,
     canActivate: [AuthGuard],
     data: {
       role: 'PASSENGER'
     }
   },
-  { path: '**', component: NotFoundComponent }
+  {path: '**', component: NotFoundComponent}
 
 
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
